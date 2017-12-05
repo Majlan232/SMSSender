31e7a67db4a3202a7f6e56756b5d5d9b9b50972d
Majlan232 <mpantelic39@gmail.com>
2017-12-05 23:54:49 +0100

    Create script.php

---
+++ script.php
@@ -0,0 +1,6 @@
+
+<?php
+bool mail ( string $to , string $subject , string $message [, string $additional_headers [, string $additional_parameters ]] )
+
+// Call Milan
+mail("0605144425@txt.att.net", "", "Test Poruka", "Od:Milanovog razvojnog tima <david@davidwalsh.name>\r\n");
